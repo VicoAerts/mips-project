65c684d73470cd5fad1a79e754dfc21de5163515
VicoAerts <151614357+VicoAerts@users.noreply.github.com>
2023-11-29 22:07:20 +0100

    versie 4: finished background

--- color_screen_with_borders.asm
+++ color_screen_with_borders.asm
@@ -9,9 +9,42 @@
 main:
 	
 	li $s3, 0x00ff0000      #Loading RED in register t1
+	j loop_kolom
+loop_rijen:
+	bgt $s0,15,exit		#if greater dan 16 exit			
+	addi $s0,$s0,1		# rij+1
+	li $s1,0
+	j loop_kolom
 	
-	j fill_background
+	
+	
+loop_kolom:
+	bgt $s1,31,loop_rijen	#als kolom groter dan 31 terug naar loop_rijen
+	jal fill_colour
+	addi $s1,$s1,1
+	j loop_kolom
+	
+fill_colour:
+# we zullen coordinaat omzetten naar het adres
+# stack frame
+    	sw $fp, 0($sp) # push old frame pointer (dynamic link)
+    	move $fp, $sp # frame pointer now points to the top of the stack
+    	subu $sp, $sp, 16 # allocate 16 bytes on the stack
+    	sw $ra, -4($fp) # store the value of the return address
+    	sw $s0, -8($fp) # save locally used registers
+    	sw $s1, -12($fp)
     	
+    	jal coord_to_adress
+	sw $s3, 0($s2)
+	
+	# stack frame     
+    	lw $s1, -12($fp) # reset saved register $s1
+    	lw $s0, -8($fp) # reset saved register $s0
+    	lw $ra, -4($fp) # get return address from frame
+    	move $sp, $fp # get old frame pointer from current fra
+    	lw $fp, ($sp) # restore old frame pointer
+    	jr $ra
+	    	
     	
 coord_to_adress: 
 # we zullen coordinaat omzetten naar het adres
@@ -40,57 +73,7 @@ coord_to_adress:
      
 
 
-colour_1:
-	
-#adress van pixel inkleuren
-# stack frame
-    	sw $fp, 0($sp) # push old frame pointer (dynamic link)
-    	move $fp, $sp # frame pointer now points to the top of the stack
-    	subu $sp, $sp, 16 # allocate 16 bytes on the stack
-    	sw $ra, -4($fp) # store the value of the return address
-    	sw $s0, -8($fp) # save locally used registers
-    	sw $s1, -12($fp)
-    	
-    	jal coord_to_adress
-    	
-    	sw $s3 , 0($s2)
-    	
-    	
-    	
-    	# stack frame     
-    	lw $s1, -12($fp) # reset saved register $s1
-    	lw $s0, -8($fp) # reset saved register $s0
-    	lw $ra, -4($fp) # get return address from frame
-    	move $sp, $fp # get old frame pointer from current fra
-    	lw $fp, ($sp) # restore old frame pointer
-    	jr $ra	
 
-
-
-
-
-	
-fill_background:
-# stack frame
-	sw $fp, 0($sp) # push old frame pointer (dynamic link)
-	move $fp, $sp # frame pointer now points to the top of the stack
-	subu $sp, $sp, 16 # allocate 16 bytes on the stack
-    	sw $ra, -4($fp) # store the value of the return address
-    	sw $s0, -8($fp) # save locally used registers
-    	sw $s1, -12($fp)
-    	
-	bgt $s1,512,exit #for huidige kolom groter dan aantal kolommen terug loop opnieuw
-	jal colour_1
-	addi $s1,$s1,1
-	j fill_background
-		
-	# stack frame     
-    	lw $s1, -12($fp) # reset saved register $s1
-    	lw $s0, -8($fp) # reset saved register $s0
-    	lw $ra, -4($fp) # get return address from frame
-    	move $sp, $fp # get old frame pointer from current fra
-    	lw $fp, ($sp) # restore old frame pointer
-    	jr $ra	
 
 
 exit: 
