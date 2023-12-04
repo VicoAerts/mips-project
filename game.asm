361f91e6ceeb20945c830f4715d6b3829b365088
VicoAerts <151614357+VicoAerts@users.noreply.github.com>
2023-12-04 23:31:26 +0100

    final commit: v1
    alles werkt nu nog opkuisen

--- game.asm
+++ game.asm
@@ -1,9 +1,12 @@
  .globl main
 
 .data
-mazeFilename:    .asciiz "C:\\Users\\Gebruiker\\Documents\\UNIF\\Jaar 1\\CSA\\CS\\input_1.txt"
+mazeFilename:    .asciiz "input_1.txt"
 buffer:          .space 4096
 victoryMessage:  .asciiz "You have won the game!"
+start_msg:	 .asciiz "geef input"
+error_msg:	 .asciiz "fout\n"
+newline: 	 .asciiz "\n"
 
 amountOfRows:    .word 16  # The mount of rows of pixels
 amountOfColumns: .word 32  # The mount of columns of pixels
@@ -31,10 +34,6 @@ main:
     	li $v0,14
     	syscall
     	move $s4,$a1
-    	#print
-la $a0,buffer
-li $v0,4
-syscall 
 
    #file is ingelezen nu maze maken 
    	li $s0,0 #rij  
@@ -42,7 +41,7 @@ syscall
    	
    		j loop_kolom
 loop_rijen:
-	bgt $s0,15,exit		#if greater dan 16 exit			
+	bgt $s0,15,main_game_loop	#if greater dan 16 exit			
 	addi $s0,$s0,1		# rij+1
 	li $s1,0
 	j loop_kolom
@@ -57,8 +56,120 @@ loop_kolom:
 	addi $s4,$s4,1 		#ga eentje verder in de file
 	addi $s1,$s1,1		#ga 1 kolom verder
 	j loop_kolom
+################################################################################################
+#bestand ingelezen en maze gemaakt	
 	
 	
+	
+change_player:
+#a0 a1 is het huidige coordinaat van de speler 
+#a2 a3 is het nieuwe coordinaat van de speler
+	move $s0,$a2 		#zet het rij nummer in het juiste register zodat we het coordinaat kunnen berekenen
+	move $s1,$a3		#zet het kolom nummer in het juiste register zodat we het coordinaat kunnen berekenen
+	jal coord_to_adress   	#bereken nu het coordinaat van de nieuwe positie
+	#s2 bevat nu het coordinaat van de nieuwe positie
+	lw $t0,0($s2)		#de kleur die op het nieuwe coord staat sit nu in t0
+	lw $t1, passageColor   	#verplaats de waarde van passage in t1
+	lw $t2, exitColor      	#verplaats de waarde van exit in t2
+	beq $t0,$t1,valid_move #als de nieuwe coordinaat een passage bevat is de move geldig 	
+	beq $t0,$t2,win_message	#als de nnieuwe coordinaat de exit bevat is de speler gewonnen
+	j wrong_coord
+	
+	
+valid_move:
+	lw $s3,playerColor		#laad terug de player color in s3
+	sw $s3, 0($s2)			#zet de speler op het nieuewe coordinaat
+	move $s0,$a0			#zet het rij nummer in het juiste register zodat we het coordinaat kunnen berekenen
+	move $s1,$a1			#zet het kolom nummer in het juiste register zodat we het coordinaat kunnen berekenen
+	jal coord_to_adress		#bereken nu het coordinaat van de nieuwe positie
+	lw $s3,passageColor		#laad de passage color op in s3
+	sw $s3, 0($s2)			#zet op het oude adress van de speler terug een passage
+	move $a0,$a2
+	move $a1,$a3			
+	j main_game_loop	
+win_message:
+#niet per se nodig maar zo zal de player echt tot de uitgang gaan ipv stoppen op 1plaats ervoor
+	lw $s3,playerColor		#laad terug de player color in s3
+	sw $s3, 0($s2)			#zet de speler op het nieuewe coordinaat
+	move $s0,$a0			#zet het rij nummer in het juiste register zodat we het coordinaat kunnen berekenen
+	move $s1,$a1			#zet het kolom nummer in het juiste register zodat we het coordinaat kunnen berekenen
+	jal coord_to_adress		#bereken nu het coordinaat van de nieuwe positie
+	lw $s3,passageColor		#laad de passage color op in s3
+	sw $s3, 0($s2)			#zet op het oude adress van de speler terug een passage
+#print de win message
+	la $a0,victoryMessage
+	li $v0,4
+	syscall
+# syscall to end the program
+    	li $v0, 10    
+    	syscall			
+wrong_coord:
+	move $a2,$a0 			#huidige plaats blijft behouden
+	move $a3,$a1				
+	j main_game_loop											
+								
+###############################################################################################
+main_game_loop:
+move $t7,$a0 #verplaats tijdellijk a0 naar t0
+# Sleep for 60 milli seconds
+	li	$a0, 60
+	li	$v0, 32
+	syscall 	
+
+#start msg
+	la $a0,start_msg	#laad de start message in a0
+	li $v0,4		#print str
+	syscall
+
+#read character
+	li $v0,12
+	syscall
+
+#move char to s0
+	move $s0,$v0
+#newline
+	la $a0,newline	#laad de newline in a0
+	li $v0,4		#print str
+	syscall
+	
+input_verwerken:
+	beq $s0,122,omhoog 		#wanneer we z krijgen beweeg omhoog 	
+	beq $s0,115,omlaag		#wanneer we s krijgen beweeeg omlaag
+	beq $s0,113,links		#wanneer we q krijgen beweeeg naar links
+	beq $s0,100,rechts		#wanneer we d krijgen beweeg naar rechts
+	beq $s0,120,einde_game		#beindig spel
+		
+error:
+	la $a0,error_msg	#laad de error message in a0
+	li $v0,4		#print str
+	syscall
+	j main_game_loop			#jump terug
+
+omhoog:
+	move $a0,$t7 			#plaats t0 terug in a0 / a terug oorspronkelijke waarde
+	subi $a2,$a0,1
+	move $a3,$a1
+	j change_player			
+omlaag:
+	move $a0,$t7 			#plaats t0 terug in a0 / a terug oorspronkelijke waarde
+	addi $a2,$a0,1
+	move $a3,$a1
+	j change_player
+links:
+	move $a0,$t7 			#plaats t0 terug in a0 / a terug oorspronkelijke waarde
+	subi $a3,$a1,1
+	move $a2,$a0
+	j change_player
+rechts:
+	move $a0,$t7 			#plaats t0 terug in a0 / a terug oorspronkelijke waarde
+	addi $a3,$a1,1
+	move $a2,$a0
+	j change_player
+einde_game:	
+	li $v0, 10		#als x da exit
+	syscall									
+											
+#######################################################################################	
 fill_colour:
 # we zullen coordinaat omzetten naar het adres
 # stack frame
@@ -121,7 +232,7 @@ check_colour:
 	beq $s5,112,colour_passage 	#wanneer de ingelezen byte p is jump naar colour passage
 	beq $s5,115,colour_player	#wanneer de ingelezen byte s is jump naar colour player
 	beq $s5,117,colour_exit		#wanneer de ingelezen byte u is jump naar colour exit
-	beq $s5,10,newline		#bij newline jumpen zonder te kleuren
+	beq $s5,10,newline_input		#bij newline jumpen zonder te kleuren
     	
     	
 	
@@ -148,7 +259,8 @@ colour_passage:
     	jr $ra
 colour_player:
 	lw $s3, playerColor      #Loading playerColor in register s2
-	
+	move $a0,$s0		 #laad de speler rij plaats in a0
+	move $a1,$s1		 #laad de speler kolom plaats in a1	
     	# stack frame     
     	lw $s1, -12($fp) # reset saved register $s1
     	lw $s0, -8($fp) # reset saved register $s0
@@ -166,7 +278,7 @@ colour_exit:
     	move $sp, $fp # get old frame pointer from current fra
     	lw $fp, ($sp) # restore old frame pointer
     	jr $ra
-newline:
+newline_input:
 	addi $s4,$s4,1 		#ga eentje verder in de file
 	j loop_kolom		#j terug naaar kolom er hoeft niks te gebeuren als er newline gelezen word
 exit:
