416e9f0face4a3a5ea6d424530fc349bc83f7038
VicoAerts <151614357+VicoAerts@users.noreply.github.com>
2023-12-13 11:52:30 +0100

    Final commit: v2
    wrong input bug fixed, added stacks.

--- game.asm
+++ game.asm
@@ -29,7 +29,7 @@ main:
     	move $a0,$v0
 
     	#load file
-    	la $a1,buffer
+    	la $a1,buffer	
     	la $a2,4096
     	li $v0,14
     	syscall
@@ -42,20 +42,20 @@ main:
    		j loop_kolom
 loop_rijen:
 	bgt $s0,15,main_game_loop	#if greater dan 16 exit			
-	addi $s0,$s0,1		# rij+1
-	li $s1,0
-	j loop_kolom
+	addi $s0,$s0,1			# rij+1
+	li $s1,0			# zet de kolomnummer terug op 0
+	j loop_kolom			#jump terug naar loop rijen om de volgende rij in te kleuren
 	
 	
 	
 loop_kolom:
-	bgt $s1,31,loop_rijen
+	bgt $s1,31,loop_rijen	#als kolom groter dan 31 terug naar loop_rijen
 	lb $s5,0($s4) 		#load wat er op deze plek in file staat
-	jal check_colour	#als kolom groter dan 31 terug naar loop_rijen
-	jal fill_colour
+	jal check_colour	#bepaal welke kleur op dit coordinaat moet komen
+	jal fill_colour		#vul deze kleur in
 	addi $s4,$s4,1 		#ga eentje verder in de file
 	addi $s1,$s1,1		#ga 1 kolom verder
-	j loop_kolom
+	j loop_kolom		#jump terug naar loop kolom
 ################################################################################################
 #bestand ingelezen en maze gemaakt	
 	
@@ -110,10 +110,16 @@ wrong_coord:
 								
 ###############################################################################################
 main_game_loop:
-move $t7,$a0 #verplaats tijdellijk a0 naar t0
+# stack frame
+    	sw $fp, 0($sp) # push old frame pointer (dynamic link)
+    	move $fp, $sp # frame pointer now points to the top of the stack
+    	subu $sp, $sp, 8 # allocate 16 bytes on the stack
+    	sw $a0, -4($fp) # store the value of a0 
+    	
+    	
 # Sleep for 60 milli seconds
-	li	$a0, 60
-	li	$v0, 32
+	li	$a0, 60		#60 milli sceonden sleep
+	li	$v0, 32		#v0 voor sleep
 	syscall 	
 
 #start msg
@@ -122,51 +128,49 @@ move $t7,$a0 #verplaats tijdellijk a0 naar t0
 	syscall
 
 #read character
-	li $v0,12
+	li $v0,12		#v0 voor read char
 	syscall
 
 #move char to s0
-	move $s0,$v0
+	move $t3,$v0
 #newline
 	la $a0,newline	#laad de newline in a0
 	li $v0,4		#print str
 	syscall
 	
+	# stack frame     
+    	lw $a0, -4($fp) # get a0 address from frame
+    	move $sp, $fp # get old frame pointer from current fra
+    	lw $fp, ($sp) # restore old frame pointer
+    	
+	
 input_verwerken:
-	beq $s0,122,omhoog 		#wanneer we z krijgen beweeg omhoog 	
-	beq $s0,115,omlaag		#wanneer we s krijgen beweeeg omlaag
-	beq $s0,113,links		#wanneer we q krijgen beweeeg naar links
-	beq $s0,100,rechts		#wanneer we d krijgen beweeg naar rechts
-	beq $s0,120,einde_game		#beindig spel
-		
-error:
-	la $a0,error_msg	#laad de error message in a0
-	li $v0,4		#print str
-	syscall
-	j main_game_loop			#jump terug
+	beq $t3,122,omhoog 		#wanneer we z krijgen beweeg omhoog 	
+	beq $t3,115,omlaag		#wanneer we s krijgen beweeeg omlaag
+	beq $t3,113,links		#wanneer we q krijgen beweeeg naar links
+	beq $t3,100,rechts		#wanneer we d krijgen beweeg naar rechts
+	beq $t3,120,einde_game		#beindig spel bij x
+	
+	j main_game_loop		#jump terug als imput niet een van bovenstaande is 
 
 omhoog:
-	move $a0,$t7 			#plaats t0 terug in a0 / a terug oorspronkelijke waarde
-	subi $a2,$a0,1
-	move $a3,$a1
+	subi $a2,$a0,1			#doe het rij nummer -1 en steek in a2
+	move $a3,$a1			#steek de oorspronkelijke rij in a3
 	j change_player			
 omlaag:
-	move $a0,$t7 			#plaats t0 terug in a0 / a terug oorspronkelijke waarde
-	addi $a2,$a0,1
-	move $a3,$a1
+	addi $a2,$a0,1			#doe het rij  numer +1 en steek in a2
+	move $a3,$a1			#steek de oorspronkelijke rij in a3
 	j change_player
 links:
-	move $a0,$t7 			#plaats t0 terug in a0 / a terug oorspronkelijke waarde
-	subi $a3,$a1,1
-	move $a2,$a0
+	subi $a3,$a1,1			#doe de kolom numer -1 en steek in a2
+	move $a2,$a0			#steek de oorspronkelijke rij in a3
 	j change_player
 rechts:
-	move $a0,$t7 			#plaats t0 terug in a0 / a terug oorspronkelijke waarde
-	addi $a3,$a1,1
-	move $a2,$a0
+	addi $a3,$a1,1			#doe de kolom numer -1 en steek in a2
+	move $a2,$a0			#steek de oorspronkelijke rij in a3
 	j change_player
 einde_game:	
-	li $v0, 10		#als x da exit
+	li $v0, 10		#als x dan exit
 	syscall									
 											
 #######################################################################################	
@@ -178,7 +182,8 @@ fill_colour:
     	subu $sp, $sp, 16 # allocate 16 bytes on the stack
     	sw $ra, -4($fp) # store the value of the return address
     	sw $s0, -8($fp) # save locally used registers
-    	sw $s1, -12($fp)
+    	sw $s1, -12($fp) 
+    	#s3 niet op stack want we hebben de waarde nodig die uit deze functie komt
     	
     	
     	jal coord_to_adress
@@ -198,10 +203,13 @@ coord_to_adress:
 # stack frame
     	sw $fp, 0($sp) # push old frame pointer (dynamic link)
     	move $fp, $sp # frame pointer now points to the top of the stack
-    	subu $sp, $sp, 16 # allocate 16 bytes on the stack
+    	subu $sp, $sp, 20 # allocate 16 bytes on the stack
     	sw $ra, -4($fp) # store the value of the return address
     	sw $s0, -8($fp) # save locally used registers
     	sw $s1, -12($fp)
+    	sw $t0,-16($fp)
+    	#s2 niet op stack want we hebben de waarde nodig die uit deze functie komt
+    	
 
 #we berekenen de offsset met formule BA+(r*K+c)*EL
     	mul $t0,$s0,32  # we zulllen het rij nummer vermenigvuldigen met de breedte nl 32
@@ -209,7 +217,9 @@ coord_to_adress:
     	mul $t0,$t0,4    #we vermenigvuldigen het nog met de lengte van elk adres wat 4 bytes is
     	add $s2,$gp,$t0  # we tellen het base adress bij de (rxK+c)*el
     
-# stack frame     
+# stack frame 
+	
+	lw $t0,-16($fp)    
     	lw $s1, -12($fp) # reset saved register $s1
     	lw $s0, -8($fp) # reset saved register $s0
     	lw $ra, -4($fp) # get return address from frame
@@ -223,10 +233,11 @@ check_colour:
     	# stack frame
     	sw $fp, 0($sp) # push old frame pointer (dynamic link)
     	move $fp, $sp # frame pointer now points to the top of the stack
-    	subu $sp, $sp, 16 # allocate 16 bytes on the stack
+    	subu $sp, $sp, 12 # allocate 16 bytes on the stack
     	sw $ra, -4($fp) # store the value of the return address
-    	sw $s0, -8($fp) # save locally used registers
-    	sw $s1, -12($fp)
+    	sw $s5, -8($fp) # save locally used registers
+    	
+    	
     	
 	beq $s5,119,colour_wall		#wanneer de ingelezen byte w is jump naar colour wall
 	beq $s5,112,colour_passage 	#wanneer de ingelezen byte p is jump naar colour passage
@@ -241,8 +252,7 @@ colour_wall:
 	lw $s3, wallColor      #Loading wallcolor in register s2
 	
     	# stack frame     
-    	lw $s1, -12($fp) # reset saved register $s1
-    	lw $s0, -8($fp) # reset saved register $s0
+    	lw $s5, -8($fp) # reset saved register $s5
     	lw $ra, -4($fp) # get return address from frame
     	move $sp, $fp # get old frame pointer from current fra
     	lw $fp, ($sp) # restore old frame pointer
@@ -251,8 +261,7 @@ colour_passage:
 	lw $s3, passageColor      #Loading RED in passageColor s2
 	
     	# stack frame     
-    	lw $s1, -12($fp) # reset saved register $s1
-    	lw $s0, -8($fp) # reset saved register $s0
+    	lw $s5, -8($fp) # reset saved register $s5
     	lw $ra, -4($fp) # get return address from frame
     	move $sp, $fp # get old frame pointer from current fra
     	lw $fp, ($sp) # restore old frame pointer
@@ -262,8 +271,7 @@ colour_player:
 	move $a0,$s0		 #laad de speler rij plaats in a0
 	move $a1,$s1		 #laad de speler kolom plaats in a1	
     	# stack frame     
-    	lw $s1, -12($fp) # reset saved register $s1
-    	lw $s0, -8($fp) # reset saved register $s0
+    	lw $s5, -8($fp) # reset saved register $s5
     	lw $ra, -4($fp) # get return address from frame
     	move $sp, $fp # get old frame pointer from current fra
     	lw $fp, ($sp) # restore old frame pointer
@@ -272,8 +280,7 @@ colour_exit:
 	lw $s3, exitColor      #Loading exitColor in register s2
 	
     	# stack frame     
-    	lw $s1, -12($fp) # reset saved register $s1
-    	lw $s0, -8($fp) # reset saved register $s0
+    	lw $s5, -8($fp) # reset saved register $s5
     	lw $ra, -4($fp) # get return address from frame
     	move $sp, $fp # get old frame pointer from current fra
     	lw $fp, ($sp) # restore old frame pointer
